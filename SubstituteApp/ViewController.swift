b3c0d16ab8bd8f32d1b8df2edb6441b1514f2333
Ashwin Gupta <gupta.ashwin08@gmail.com>
2020-05-12 19:25:30 +1000

    Initial Commit

---
+++ SubstituteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubstituteApp
+//
+//  Created by Ashwin Gupta on 12/5/20.
+//  Copyright © 2020 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
